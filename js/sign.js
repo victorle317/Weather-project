dd077c718fce6c7f28076d4aa1e97c76610f4186
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-10-29 15:30:49 +0700

    update by pastetu

--- js/sign.js
+++ js/sign.js
@@ -1,5 +1,4 @@
 const $toggleFunc = document.getElementsByClassName("toggleFunc");
-console.log($toggleFunc.length);
 for (let i = 0; i < $toggleFunc.length; i++) {
   $toggleFunc[i].addEventListener("click", (e) => {
     e.preventDefault();
