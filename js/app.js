084e1f05e395260d8911491c8a5cba196c07d882
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-10-29 17:29:04 +0700

    update FE

--- js/app.js
+++ js/app.js
@@ -1,49 +1,39 @@
 import "./sign.js";
 
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 
+// (function ($, document, window) {
+//   $(document).ready(function () {
+//     // Cloning main navigation for mobile menu
+//     $(".mobile-navigation").append($(".main-navigation .menu").clone());
 
+//     // Mobile menu toggle
+//     $(".menu-toggle").click(function () {
+//       $(".mobile-navigation").slideToggle();
+//     });
 
+//     var map = $(".map");
+//     var latitude = map.data("latitude");
+//     var longitude = map.data("longitude");
+//     if (map.length) {
+//       map.gmap3({
+//         map: {
+//           options: {
+//             center: [latitude, longitude],
+//             zoom: 15,
+//             scrollwheel: false,
+//           },
+//         },
+//         marker: {
+//           latLng: [latitude, longitude],
+//         },
+//       });
+//     }
+//   });
 
-
-
-
-
-
-
-
-
-
-
-(function ($, document, window) {
-  $(document).ready(function () {
-    // Cloning main navigation for mobile menu
-    $(".mobile-navigation").append($(".main-navigation .menu").clone());
-
-    // Mobile menu toggle
-    $(".menu-toggle").click(function () {
-      $(".mobile-navigation").slideToggle();
-    });
-
-    var map = $(".map");
-    var latitude = map.data("latitude");
-    var longitude = map.data("longitude");
-    if (map.length) {
-      map.gmap3({
-        map: {
-          options: {
-            center: [latitude, longitude],
-            zoom: 15,
-            scrollwheel: false,
-          },
-        },
-        marker: {
-          latLng: [latitude, longitude],
-        },
-      });
-    }
-  });
-
-  $(window).load(function () {});
-})(jQuery, document, window);
-
+//   $(window).load(function () {});
+// })(jQuery, document, window);
---
+++ js/test.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    let menuToggle = document.querySelector(".toggle");
+    let menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+  }
+  
