db6c2407d5077c454fd75e3a5333505bec53a263
victorle317 <lechienthang2k2000@gmail.com>
2020-11-03 21:14:51 +0700

    bug fixed

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,38 @@
-!function($,n,a){$(n).ready(function(){$(".mobile-navigation").append($(".main-navigation .menu").clone()),$(".menu-toggle").click(function(){$(".mobile-navigation").slideToggle()});var n=$(".map"),a=n.data("latitude"),o=n.data("longitude");n.length&&n.gmap3({map:{options:{center:[a,o],zoom:15,scrollwheel:!1}},marker:{latLng:[a,o]}})}),$(a).load(function(){})}(jQuery,document,window);
+(function ($, document, window) {
+
+    $(document).ready(function () {
+
+        // Cloning main navigation for mobile menu
+        $(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+        // Mobile menu toggle 
+        $(".menu-toggle").click(function () {
+            $(".mobile-navigation").slideToggle();
+        });
+
+        var map = $(".map");
+        var latitude = map.data("latitude");
+        var longitude = map.data("longitude");
+        if (map.length) {
+
+            map.gmap3({
+                map: {
+                    options: {
+                        center: [latitude, longitude],
+                        zoom: 15,
+                        scrollwheel: false
+                    }
+                },
+                marker: {
+                    latLng: [latitude, longitude],
+                }
+            });
+
+        }
+    });
+
+    $(window).on('load', function () {
+
+    });
+
+})(jQuery, document, window);
