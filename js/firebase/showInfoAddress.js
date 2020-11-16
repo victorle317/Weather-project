46838b7f6fa8f979805f29e03b23c4b21d669d98
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-11-16 18:32:50 +0700

    .commit

--- js/firebase/showInfoAddress.js
+++ js/firebase/showInfoAddress.js
@@ -22,10 +22,5 @@ let showInfoAddress = (user) => {
         }
       });
     });
-    // let result = await db.collection("Addresses").doc(id).get();
-    // let objectData = result.data();
-    // console.log(0, objectData);
-    // if (Object.getOwnPropertyNames(objectData).length !== 0)
-    //   template.showData(objectData);
   })(user.uid);
 };
